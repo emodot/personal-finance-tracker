803ef7eeb61c620158cf0aea81b03aa63af0811f
emodot <emayeodavid@gmail.com>
2024-11-14 17:29:17 +0100

    feat: developed and implemented Add transactions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { saveTransactions, loadTransactions } from "./utils/store";
+import AddTransaction from "./components/AddTransaction";
+
 
 function App() {
   const [transactions, setTransactions] = useState(loadTransactions());
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     saveTransactions(transactions);
   }, [transactions]);
 
   const addTransaction = (transaction) => {
+    console.log(transaction);
+    
     setTransactions([transaction, ...transactions]);
   };
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <h1 className="text-2xl font-bold text-center">
-        David's Finance Tracker
-      </h1>
+    <div className="pt-[3rem] max-w-[1350px] m-auto min-h-screen bg-gray-100 p-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-[20px] font-qanelas_b text-center">
+          David's Finance Tracker
+        </h1>
+        <button
+          onClick={openModal}
+          className="bg-primary_color text-white font-qanelas_m px-4 py-2 rounded hover:bg-blue-600"
+        >
+          Add Transaction
+        </button>
+      </div>
+      <AddTransaction
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        onAddTransaction={addTransaction}
+      />
     </div>
   );
 }
---
+++ src/components/AddTransaction.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+
+const AddTransaction = ({ isOpen, onClose, onAddTransaction }) => {
+  const [formData, setFormData] = useState({
+    type: "",
+    amount: "",
+    date: "",
+    category: "",
+    notes: "",
+  });
+
+  const addTransaction = (e) => {
+    e.preventDefault();
+    if (!formData.type || !formData.amount || !formData.date || !formData.category) return;
+    const payload = { ...formData, amount: parseFloat(formData.amount) };
+    onAddTransaction(payload);
+    onClose();
+    setFormData({
+      date: "",
+      category: "",
+      type: "income",
+      amount: "",
+      notes: "",
+    });
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  if (!isOpen) return null;
+  return (
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white p-6 rounded-lg border border-neutral_stroke_2 w-[30%] relative">
+        <button
+          onClick={onClose}
+          className="text-gray-500 font-qanelas_b hover:text-error absolute top-4 right-4"
+        >
+          Close
+        </button>
+        <h2 className="text-[20px] font-qanelas_b text-center mb-4">
+          Add Transaction
+        </h2>
+
+        <form
+          onSubmit={addTransaction}
+          className="p-4 bg-white rounded space-y-4"
+        >
+          <div>
+            <label className="text-sm font-medium">Transaction Type</label>
+            <select
+              value={formData.type}
+              name="type"
+              onChange={handleChange}
+              className="w-full p-2 border rounded-md"
+            >
+              <option value="">Select Type</option>
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+          </div>
+          <div>
+            <label className="text-sm font-medium">Amount</label>
+            <input
+              type="number"
+              value={formData.amount}
+              name="amount"
+              onChange={handleChange}
+              className="w-full p-2 border rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="text-sm font-medium">Date</label>
+            <input
+              type="date"
+              value={formData.date}
+              name="date"
+              onChange={handleChange}
+              className="w-full p-2 border rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="text-sm font-medium">Category</label>
+            <input
+              type="text"
+              value={formData.category}
+              name="category"
+              onChange={handleChange}
+              className="w-full p-2 border rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="text-sm font-medium">Notes (optional)</label>
+            <textarea
+              value={formData.notes}
+              name="notes"
+              onChange={handleChange}
+              id="notes"
+              rows={4}
+              className="w-full p-2 border rounded-md"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="bg-primary_color text-white py-2 px-4 rounded-md"
+          >
+            Add Transaction
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddTransaction;
